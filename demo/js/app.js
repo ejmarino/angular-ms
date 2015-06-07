79a0f57080a8cfbc2b190e4d4f8f73c5cc3e3083
Emiliano Marino <emiliano@ejmarino.com>
2015-06-07 02:00:03 -0300

    Nice demostration page with sb admin theme

---
+++ demo/js/app.js
@@ -0,0 +1,113 @@
+/* global angular */
+(function () {
+  'use strict';
+
+  // Declare the main module
+  var myApp = angular.module('myApp', ['ngms']);
+
+
+  myApp.run(function ($interval, dataService) {
+    $interval(function() {
+      dataService.add(dataService.buildRandomItem());
+    }, 2000);    
+  });
+
+  myApp.controller('panelsController', function ($scope, ngmsMessageService, dataService) {
+
+    var channel = ngmsMessageService.getChannel('ModelChanges');
+
+    $scope.totalItems = dataService.count();
+    $scope.newestDate = '-';
+    $scope.tempPeaks = 0;
+    $scope.humPeaks = 0;
+    $scope.badDays = 0;
+
+    channel.subscribe('new', function(message) {
+      $scope.totalItems = message.count;
+      $scope.newestDate = message.data.timestamp.toLocaleTimeString();
+      if (message.data.temperature > 35) {
+        $scope.tempPeaks++;
+      }
+      if (message.data.humidity > 90) {
+        $scope.humPeaks++;
+      }
+      if (message.data.humidity > 90 && message.data.temperature > 35) {
+        $scope.badDays++;
+      }
+    }); 
+
+    channel.subscribe('update', function(message) {
+      $scope.newestDate = message.data.timestamp.toLocaleTimeString();
+      if (message.data.temperature > 35) {
+        $scope.tempPeaks++;
+      }
+      if (message.data.humidity > 90) {
+        $scope.humPeaks++;
+      }
+      if (message.data.humidity > 90 && message.data.temperature > 35) {
+        $scope.badDays++;
+      }
+    }); 
+
+    $scope.$on('destroy', function() {
+      channel.unsubscribeAll();
+    });
+
+  });
+
+  myApp.controller('messagesController', function ($scope, ngmsMessageService) {
+
+    var channel = ngmsMessageService.getChannel('ModelChanges');
+
+    $scope.itemsList = [];
+
+    channel.subscribe('new', function (message) {
+      $scope.itemsList.push(message.data);
+      if ($scope.itemsList.length > 4) {
+        $scope.itemsList = $scope.itemsList.splice(1); 
+      }
+    });
+
+    $scope.$on('destroy', function() {
+      channel.unsubscribeAll();
+    });
+
+  });
+
+
+
+  myApp.controller('graphController', function ($scope, ngmsMessageService) {
+
+    var channel = ngmsMessageService.getChannel('ModelChanges');
+    var data = [];
+
+    var graph = Morris.Area({
+              element: 'morris-area-chart',
+              data: [],
+              xkey: 'date',
+              ykeys: ['temperature', 'humidity'],
+              labels: ['Temperature', 'Humidity'],
+              pointSize: 2,
+              hideHover: 'auto',
+              resize: true
+          });
+
+    channel.subscribe('new', function (message) {
+      var item = angular.copy(message.data);
+      item.date = item.timestamp.toJSON();
+      data.push(item);
+      if (data.length > 20) {
+        data = data.splice(1); 
+      }
+      graph.setData(data);
+       
+    });
+
+    $scope.$on('destroy', function() {
+      channel.unsubscribeAll();
+    });
+
+  });
+
+
+} ());
---
+++ demo/js/dataservice.js
@@ -0,0 +1,73 @@
+(function () {
+  'use strict';
+
+  var myApp = angular.module('myApp');
+
+  myApp.factory('dataService', function (ngmsMessageService) {
+
+    var data = [];
+    var len = 30;
+
+    var startTimestamp = new Date().getTime() - 86400 * len;
+
+    for (var i = 0; i < len; i++) {
+      data[i] = {
+        id: i+1,
+        timestamp: new Date(startTimestamp + i*86400),
+        temperature: i * 0.3 + 15 + Math.random() * 15,
+        humidity: 50 + Math.random() * 50
+      };
+    }
+
+    var channel = ngmsMessageService.getChannel('ModelChanges');
+
+    function buildRandomItem() {
+      return {
+        id: 0,
+        timestamp: new Date(),
+        temperature: 25 + Math.random() * 15,
+        humidity: 50 + Math.random() * 50
+      };
+    }
+
+    function add(item) {
+      var newitem = angular.copy(item);
+       data.push(newitem);
+       newitem.id = data.length;
+       newitem.timestamp = new Date();
+       channel.publish('new',{data: item, count: data.length});
+       return item.id;
+    }	
+
+    function edit(item) {
+      var newitem = angular.copy(item);
+       data[newitem.id] = newitem;
+       newitem.timestamp = new Date();
+       channel.publish('update',{data: newitem, count: data.length});
+    }	
+
+    function get(id) {
+       return angular.copy(data[id]);
+    }
+
+    function query() {
+       return angular.copy(data);
+    }
+
+    function count() {
+       return data.length;
+    }
+    
+    return {
+      query: query,
+      get: get,
+      count: count,
+      add: add,
+      edit: edit,
+      buildRandomItem: buildRandomItem
+    };
+
+  });
+
+
+} ());
