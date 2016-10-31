2226ef9b6a8d7ea006578d4f31ec503ebb182f30
Emiliano Marino <emiliano@ejmarino.com>
2016-10-31 14:53:11 -0300

    Se corrigio el main e typings en package.json

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,4 +1,4 @@
-/// <reference path="globals/jquery/index.d.ts" />
-/// <reference path="globals/angular/index.d.ts" />
 /// <reference path="globals/angular-mocks/index.d.ts" />
+/// <reference path="globals/angular/index.d.ts" />
 /// <reference path="globals/jasmine/index.d.ts" />
+/// <reference path="globals/jquery/index.d.ts" />
